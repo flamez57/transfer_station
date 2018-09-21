fdacb1be539e1d0c63879c291365e2600f82d596
flamez <1050355217@qq.com>
2018-09-21 15:48:52 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,30 @@
+exports.types = {
+ "css": "text/css",
+ "gif": "image/gif",
+ "html": "text/html",
+ "htm": "text/html",
+ "ico": "image/x-icon",
+ "jpeg": "image/jpeg",
+ "jpg": "image/jpeg",
+ "js": "text/javascript",
+ "json": "application/json",
+ "pdf": "application/pdf",
+ "png": "image/png",
+ "svg": "image/svg+xml",
+ "swf": "application/x-shockwave-flash",
+ "tiff": "image/tiff",
+ "txt": "text/plain",
+ "wav": "audio/x-wav",
+ "wma": "audio/x-ms-wma",
+ "wmv": "video/x-ms-wmv",
+ "xml": "text/xml"
+};
+exports.netServerUrlFlag = "NETSERVER";
+exports.netServerhost = "";
+exports.netServerport = "";
+exports.javaServerUrlFlag = "JAVASERVER";
+exports.javaServerhost = ""; //转发的地址
+exports.javaServerport = "";//转发的端口
+exports.fileServerUrlFlag="FileUpload";
+exports.webapp = "public";//项目目录
+exports.webport = "82"; //项目启动端口
